9da46a41628d00572a29cd148e96111de2e0a29f
Joel oliveira <oliveira.wjc@gmail.com>
2025-11-18 09:16:52 -0300

    função ver e editar

--- tarefas.py
+++ tarefas.py
@@ -26,3 +26,47 @@ def remover():
     
     if not encontrada:
         print(f"Tarefa '{remover_tarefa}' não encontrada.")
+
+def ver():
+    print("\n--- Lista de Tarefas ---")
+    if not lista_tarefas:
+        print("A lista está vazia.")
+    else:
+        for i in range(len(lista_tarefas)):
+            print(f"Registro {i+1}:")
+            print("  Tarefa:", lista_tarefas[i]["Tarefa"])
+            print("  Valor:", lista_tarefas[i]["Valor"])
+            print("-----")
+
+
+def edite():
+    print("\n-Editar Tarefa")
+    if not lista_tarefas:
+        print("A lista está vazia. Nada para editar.")
+        return
+    
+    tarefa_editar = input("Digite a tarefa que você quer editar: ")
+    encontrada = False
+
+    for i in range(len(lista_tarefas)):
+        if lista_tarefas[i]["Tarefa"] == tarefa_editar:
+            print("1. Editar descrição")
+            print("2. Editar valor")
+            opcao = input("Escolha (1 ou 2): ")
+            
+            if opcao == "1":
+                nova_tar = input("Digite a nova tarefa: ")
+                lista_tarefas[i]["Tarefa"] = nova_tar
+                print("Tarefa atualizada.")
+            elif opcao == "2":
+                novo_valor = input("Digite o novo valor: ")
+                lista_tarefas[i]["Valor"] = novo_valor
+                print("Valor atualizado.")
+            else:
+                print("Opção inválida.")
+            
+            encontrada = True
+            break
+
+    if not encontrada:
+        print(f"Tarefa '{tarefa_editar}' não encontrada.")
