816b04e307de489327a03e943eaacbaa0173c2bd
Du Boxwell <ebs2@cesar.school>
2025-11-24 19:07:43 -0300

    easter_egg.py

--- easter_egg.py
+++ easter_egg.py
@@ -1,3 +1,3 @@
 def checar_easter_egg(eventos):
-    if len(eventos) == 25:
-        print("\n🎂 Parabéns, este é o vigésimo quinto evento. Eu até diria que você merece um bolo, se ele não fosse uma mentira. ;D 🎂")
+    if len(eventos) == 159:
+        print("\n🎂 Parabéns, este é o centésimo quinquagésimo evento. Eu até diria que você merece um bolo, se ele não fosse uma mentira. ;D 🎂")
